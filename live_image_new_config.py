1ce8443cf0b1cd7babfa5cd787052d2c8ace96e0
Henry Smith <henryjsmith12@outlook.com>
2023-01-30 10:44:49 -0600

    Add global config dict

---
+++ live_image_new_config.py
@@ -0,0 +1,22 @@
+# =====================================================================
+# Global configuration dictionary used for all PV reading
+
+CONFIG = {
+    "HKL_MODE": True, "ROI_MODE": True,
+    "DET_PRESENT": False, "INSTR_PRESENT": False, "ROI_PRESENT": False, "ENERGY_PRESENT": False,
+    "IMAGE_PV": None, "IMAGE_TOTAL_PV": None, "IMAGE_MAX_PV": None, 
+    "PIXEL_DIR_1": None, "PIXEL_DIR_2": None,
+    "C_CH_1": None, "C_CH_2": None,
+    "N_CH_1": None, "N_CH_2": None,
+    "PIXEL_WIDTH_1": None, "PIXEL_WIDTH_2": None,
+    "DISTANCE": None,
+    "DET_ROI": None,
+    "SAMPLE_CIRCLE_DIR": None, "SAMPLE_CIRCLE_NAMES": None, "SAMPLE_CIRCLE_PV_LIST": None,
+    "DET_CIRCLE_DIR": None, "DET_CIRCLE_NAMES": None, "DET_CIRCLE_PV_LIST": None,
+    "CIRCLE_PV_LIST": None,
+    "PRIMARY_BEAM_DIR": None, "INPLANE_REF_DIR": None, "SAMPLE_NORM_DIR": None,
+    "Q_CONV": None,
+    "UB_MATRIX_PV": None,
+    "ROI_PV_LIST": None,
+    "ENERGY_PV": None
+}
